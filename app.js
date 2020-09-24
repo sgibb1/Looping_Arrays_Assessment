6ef437aa1dd90b49c80464c2e5d68cf36d289c60
jahairabarrett <70175722+jahairabarrett@users.noreply.github.com>
2020-09-23 20:43:17 -0400

    Countdown and Array of Favorite TV Shows Added

--- app.js
+++ app.js
@@ -21,3 +21,19 @@ for (let y = 0, y <= 10001; y++) {
 }
 
 //Finally write an array of your top 5 favorite tv shows (make them up if you don't have any) Loop through the array of your top 5 favorite tv shows and log to the console the following message each loop:"My # favorite tv show is "
+
+================================== Countdown and Array of Favorite TV Shows
+
+//Write a for loop that logs every even number starting from 1000 down to 0
+
+for(var i= 1000; i>=0; i-=2)
+   console.log(i)
+
+
+//Write an array of your top 5 favorite tv shows
+
+var shows = ["My #1 favorite tv show is The Office", "My #2 favorite tv show is 90s Charmed","My #3 favorite tv show is The Wayans Bros","My #4 favorite tv show is Living Single","My #5 favorite tv show is New Girl"];
+for (var i=0; i<6; i++){
+  console.log(shows[i]);
+}
+
