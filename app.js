405ca80897731130c302e05816b9e9f5509570f6
Shaun Gibb <sgibb1@mail.valenciacollege.edu>
2020-09-25 13:00:44 -0400

    changed code for alert prompt

--- app.js
+++ app.js
@@ -8,21 +8,11 @@ for (let i = 1000; i > 0; i--) {
 
 // Write a for loop that starts at 0 and goes to 10000, don't log any numbers but when the for loop is 1/4 of the way done through the loops (2500), your code should alert a message that says "A quarter of the way there!" then when your loop is halfway there (5000), your code should alert a message that says "Halfway there!" Finally when it reaches the last number (10000, make sure your condition is inclusive of 10000) it should alert "The loop is done!"
 
-var startValue = 0
-var endValue = 10000
-
-for (startValue; startValue < endValue; startValue++) {
-  if (startValue === 2500) {
-    alert("A quarter of the way there!")
-  }
-  else if (startValue === 5000) {
-    alert("Halfway there!")
-  }
-  else if (startValue === 10000) {
-    alert("The loop is done!")
-  }
+for (let i = 0; i <= 10000; i++) {
+  if (i == "2500") alert("A quarter of the way there!");
+  else if (i == "5000") alert("Halfway there!");
+  else if (i == "10000") alert("The loop is done!");
 }
-
 
 //Finally write an array of your top 5 favorite tv shows (make them up if you don't have any) Loop through the array of your top 5 favorite tv shows and log to the console the following message each loop:"My # favorite tv show is "
 
